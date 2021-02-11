c3b48061310c82b02f111f4d9c7475b684ffe768
William Wen <wwwenwilliam@gmail.com>
2021-02-10 22:03:19 -0500

    add bubblesort with >1 dimension

--- Sorts/bubblesort.py
+++ Sorts/bubblesort.py
@@ -13,8 +13,25 @@ def bubbleSort(passedList):
         if isSorted:
             break
     return passedList
+
+def bubbleSortDimensional(column, passedList):
+    #takes a list
+    #returns sorted version of list
+    limit = len(passedList)
+    for i in range(limit):
+        isSorted = True
+        for j in range(limit-1):
+            if passedList[j][column] > passedList[j+1][column]:
+                (passedList[j], passedList[j+1]) = (passedList[j+1], passedList[j])
+                isSorted = False
+        if isSorted:
+            break
+    return passedList
     
 
 ##test program
 numList = [7, 13, 15, 3, 6, 8]
 print(bubbleSort(numList))
+
+myList = [[ "Adam","Math",90 ], [ "Mike","English",70 ], [ "Bing Xin","CompSci",100 ]]
+print(bubbleSortDimensional(0, myList))
