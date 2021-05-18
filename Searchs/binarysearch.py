c9bcccfa85d9d1d65af5eea163d1e6a13493e240
William Wen <littlepawn2@gmail.com>
2021-05-17 20:59:27 -0400

    fixed issues with binary search

--- Searchs/binarysearch.py
+++ Searchs/binarysearch.py
@@ -2,23 +2,23 @@ def binarySearch(passedList, searchItem):
     #takes sorted list and an item
     #returns position of item in list
     top = 0
-    bottom = len(passedList)
+    bottom = len(passedList)-1
     middle = (bottom + top) // 2
     
-    if passedList[middle] == searchItem:
-        return middle
+    if len(passedList) == 0:
+        return -1
     
-    while top != bottom:
+    while top <= bottom:
         if passedList[middle] < searchItem:
             top = middle + 1
-        else: 
+        elif passedList[middle] > searchItem: 
             bottom = middle - 1
+        else:
+            return middle
         middle = (bottom + top) // 2
-    if passedList[middle] == searchItem:
-        return middle
     else:
         print("Item not found")
         return None
     
-testList = ["A", "B", "C", "D"]
-print(binarySearch(testList, "C"))
+testList = []
+print(binarySearch(testList, 0))
